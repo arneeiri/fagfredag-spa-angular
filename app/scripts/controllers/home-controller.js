4899e727f1cf8d53cf26f3ee70ff94373c4dc4a2
Arne Eirik Nielsen <arneeiri@gmail.com>
2013-11-08 13:57:21 +0100

    Added more stuff

--- app/scripts/controllers/home-controller.js
+++ app/scripts/controllers/home-controller.js
@@ -1,3 +1,16 @@
-angular.module('yrApp').controller('HomeCtrl', function ($scope, sampleService) {
-  $scope.message = sampleService.sayHello();
+angular.module('yrApp').controller('HomeCtrl', function ($scope, yrService) {
+    yrService.getForecast({place: 'Oslo', municipality: 'Oslo', county: 'Oslo'}).then(function(forecast) {
+        console.log(forecast);
+        $scope.forecast = forecast;
+        forecast.periods[0].selected = true;
+        $scope.nowPeriod = forecast.periods[0];
+  });
+
+    $scope.select = function(period) {
+        $.each($scope.forecast.periods, function(i) {
+           $scope.forecast.periods[i].selected = false;
+        });
+        period.selected = true;
+        console.log('clack');
+    }
 });
