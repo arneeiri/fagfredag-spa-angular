9975f51fed22ece223525c7de543d74b2aaf1a04
Arne Eirik Nielsen <arneeiri@gmail.com>
2013-11-08 14:31:58 +0100

    Added search for place

--- app/scripts/controllers/home-controller.js
+++ app/scripts/controllers/home-controller.js
@@ -1,9 +1,8 @@
-angular.module('yrApp').controller('HomeCtrl', function ($scope, yrService) {
+angular.module('yrApp').controller('HomeCtrl', function ($scope, yrService, searchService) {
     yrService.getForecast({place: 'Oslo', municipality: 'Oslo', county: 'Oslo'}).then(function(forecast) {
         console.log(forecast);
         $scope.forecast = forecast;
         forecast.periods[0].selected = true;
-        $scope.nowPeriod = forecast.periods[0];
   });
 
     $scope.select = function(period) {
@@ -11,6 +10,22 @@ angular.module('yrApp').controller('HomeCtrl', function ($scope, yrService) {
            $scope.forecast.periods[i].selected = false;
         });
         period.selected = true;
-        console.log('clack');
+    }
+
+    $scope.search = function(q) {
+        searchService.search(q).then(function(result) {
+            $scope.searchResult = result;
+            console.log(result);
+        });
+    }
+
+    $scope.getWeather = function(location) {
+        $scope.q = '';
+        $scope.searchResult = null;
+        yrService.getForecast({place: location.name, municipality: location.municipality, county: location.county}).then(function(forecast) {
+            console.log(forecast);
+            $scope.forecast = forecast;
+            forecast.periods[0].selected = true;
+        });
     }
 });
